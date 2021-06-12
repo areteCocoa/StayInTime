55f5b1b13730a5b0bd5dcac09afda98b4d40502a
Thomas Ring <thomasjring@icloud.com>
2021-06-12 13:52:29 -0700

    Initial Commit

---
+++ SmartMetronome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartMetronome
+//
+//  Created by Thomas Ring on 6/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartMetronome/SmartMetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMetronomeApp.swift
+//  SmartMetronome
+//
+//  Created by Thomas Ring on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SmartMetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
